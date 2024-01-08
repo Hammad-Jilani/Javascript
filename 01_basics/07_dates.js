97833e522d368b2107a1b312a5c009d0712a1cf0
Hammad-Jilani <155903223+Hammad-Jilani@users.noreply.github.com>
2024-01-08 18:56:59 +0000

    Dates and it's methods

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,21 @@
+let myDate = new Date();
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+
+// let myCreatedDate = new Date(2023,0,23);
+// console.log(myCreatedDate.toLocaleString());
+let myCreatedDate2 = new Date("2023-01-10");
+// console.log(myCreatedDate2.toLocaleString());
+// let myCreatedDate3 = new Date(2023,1,28,13,39);
+// console.log(myCreatedDate3.toDateString());
+// console.log(myCreatedDate3.toLocaleString());
+
+let myTimeStamp = Date.now();
+// console.log(myTimeStamp);
+// console.log(myCreatedDate2.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+
+let newDate = new Date();
+console.log(newDate.getMonth()+1);
