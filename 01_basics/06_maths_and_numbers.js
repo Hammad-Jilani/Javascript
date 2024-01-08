23967351a19c45d85c6081946a4372227ede7159
Hammad-Jilani <155903223+Hammad-Jilani@users.noreply.github.com>
2024-01-08 17:43:36 +0000

    Math and Number

---
+++ 01_basics/06_maths_and_numbers.js
@@ -0,0 +1,27 @@
+const score = 400;
+
+const balance = new Number(100);
+// console.log(balance);
+
+// console.log(balance.toString());
+// console.log(balance.toFixed(2));
+const otherNumber = 123.4385794;
+// console.log(otherNumber.toPrecision(6));
+const hundred = 1000000;
+// console.log(hundred.toLocaleString());
+
+// ++++++++++++++++===========>>>>>>>>>>>>>>>>>>>
+// console.log(Math);
+// console.log(Math.abs(-5));
+// console.log(Math.min(94,3,1,2,4,21));
+// console.log(Math.max(9546,324,212,42,1,421));
+// console.log(Math.round(8.9));
+// console.log(Math.ceil(8.1));
+// console.log(Math.floor(29.9));
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+console.log(Math.floor((Math.random()*10)+1));
+const max = 20;
+const min = 10;
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
