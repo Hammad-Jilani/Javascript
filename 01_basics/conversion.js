9346c5cca55a0c5d072517ec5b0dd0bc126bde33
Hammad-Jilani <155903223+Hammad-Jilani@users.noreply.github.com>
2024-01-08 10:22:30 +0000

    conversion of datatypes in javaScript

---
+++ 01_basics/conversion.js
@@ -0,0 +1,21 @@
+let score  = "33";
+console.log(typeof score);
+
+// let NumberInString = Number(score);
+// console.log(typeof NumberInString);
+// "33" => 33
+// "41abcd" =>NaN
+// true => 1 ; false => 0
+
+let isBoolean = 1;
+
+// let toBoolean  = Boolean(isBoolean);
+// console.log(toBoolean);
+
+// 1 => true; 0=> false;
+// "" => false;
+
+let someNumber = 31;
+
+console.log(typeof someNumber);
+let toString = String(someNumber);
